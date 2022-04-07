d17546323b2fbcfafe5198020607bb8b8eab8c21
Jen Villaganas <jenvillaganas@gmail.com>
2022-04-07 14:59:06 +0800

    Trigger callback on postmessage

--- index.js
+++ index.js
@@ -12,21 +12,36 @@ class WalletKit {
     document.addEventListener("DOMContentLoaded", (event) => {
       window.walletkit = this
       this.el = document.querySelector(this.elSelector)
+      this.setBaseUrl()
       this.addListeners()
     });
   }
 
+  setBaseUrl() {
+    this.appBaseUrlObject = new URL(this.appBaseUrl)
+    this.appBaseUrlObject.pathname = "connect"
+    this.appBaseUrlObject.searchParams.set('redirect_url', this.authRedirectUrl)
+    this.appBaseUrlObject.searchParams.set('popout', true)
+  }
+
   triggerAfterSignIn(userParams) {
-    this.afterSignInCallback(userParams)
+    this.afterSignInCallback({...userParams})
   }
 
   addListeners() {
     this.el.addEventListener('click', () => {
-      let url = new URL(this.appBaseUrl)
-      url.pathname = "connect"
-      url.searchParams.set('redirect_url', this.authRedirectUrl)
-      url.searchParams.set('popout', true)
-      this.sodakitWindow = window.open(url, 'sodakitWindow', 'height=500,width=500')
+      this.sodakitWindow = window.open(this.appBaseUrlObject, 'sodakitWindow', 'height=500,width=500')
     })
+
+    window.addEventListener("message", (event) => {
+      if (event.origin !== this.appBaseUrlObject.origin)
+        return
+
+
+      if (event.data.event_name == "after_signin") {
+        this.triggerAfterSignIn(event.data)
+      }
+    }, false);
+
   }
 }
